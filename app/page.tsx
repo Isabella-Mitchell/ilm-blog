1e4c20d1c31f6e19236d01e42edc7604fcd92ad7
Isabella Mitchell <i.mitchell.0707@gmail.com>
2025-02-14 12:41:33 +0000

    Personalises template

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import { allPosts } from "@/.contentlayer/generated"
-import Link from "next/link"
+import { allPosts, Post } from "@/.contentlayer/generated";
+import Link from "next/link";
+
+const formatToDate = (post: Post): any => {
+  return { ...post, date: new Date(post.date) };
+};
+
+const formatToString = (post: any): Post => {
+  return { ...post, date: post.date.toString() };
+};
 
 export default function Home() {
+  const allPostsOrdered = [...allPosts]
+    .sort((post1, post2) => formatToDate(post2).date - formatToDate(post1).date)
+    .map((post) => formatToString(post));
+
   return (
     <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
+      {allPostsOrdered.map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
             <h2>{post.title}</h2>
@@ -13,5 +25,5 @@ export default function Home() {
         </article>
       ))}
     </div>
-  )
+  );
 }
